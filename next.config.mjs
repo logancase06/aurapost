b9434b8ecd1319e7b34455be58672b87a0382284
Logan <logan.case06@gmail.com>
2026-06-28 14:22:19 +0200

    fix(csp): ajouter fonts.googleapis.com a style-src (Crisp charge Mulish/Inter via Google Fonts)

--- next.config.mjs
+++ next.config.mjs
@@ -49,7 +49,7 @@ const nextConfig = {
     const csp = [
       "default-src 'self'",
       "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com https://client.crisp.chat https://www.googletagmanager.com https://connect.facebook.net",
-      "style-src 'self' 'unsafe-inline'",
+      "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       "img-src 'self' data: blob: https:",
       "font-src 'self' data: https://fonts.gstatic.com",
       "connect-src 'self' https: wss://client.relay.crisp.chat wss://stream.crisp.chat https://fonts.gstatic.com https://fonts.googleapis.com",
